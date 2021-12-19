fc8a1014fbe007806c67088497bbfbf1cb2e66d3
ruvian <51181973+RuvianFabrin@users.noreply.github.com>
2021-12-19 17:02:35 -0300

    Aula ponteiros

---
+++ 8-Ponteiros/ponteiros.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Ponteiros")
+	var variavel1 int = 10
+	var variavel2 int = variavel1
+
+	fmt.Println(variavel1, variavel2)
+
+	variavel1++
+	fmt.Println(variavel1, variavel2)
+
+	//PONTEIRO É UMA REFERÊNCIA DE MEMÓRIA
+	var variavel3 int
+	var ponteiro *int
+
+	fmt.Println(variavel3, ponteiro)
+
+	variavel3 = 100
+	ponteiro = &variavel3
+
+	//Vê o endereço de memória
+	fmt.Println(variavel3, ponteiro)
+
+	//Vê o valor dentro do ponteiro //desferênciação
+	fmt.Println(variavel3, *ponteiro)
+
+	variavel3 = 150
+	fmt.Println(variavel3, *ponteiro)
+}
