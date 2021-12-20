b0b08f233a6d8e553e9526be9be753369f95f6a9
ruvian <51181973+RuvianFabrin@users.noreply.github.com>
2021-12-20 15:47:46 -0300

    Lição Retorno Nomeado

---
+++ 14-Funções Avançadas/14.1-Retorno Nomeado/retorno-nomeado.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func calculosmatematicos(n1, n2 int) (soma int, subtracao int) {
+	soma = n1 + n2
+	subtracao = n1 - n2
+	return
+
+}
+func main() {
+	fmt.Println("Retorno Nomeado")
+	soma, subtracao := calculosmatematicos(10, 20)
+	fmt.Println(soma, subtracao)
+}
